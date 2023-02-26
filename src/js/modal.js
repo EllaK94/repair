8285b1a1c850abd039089300a40d33f3db75307c
Alla <ella.rock@mail.ru>
2023-02-26 15:46:27 +0300

    Модальное окно открывается и закрывается с помощью библиотеки jQuery (добавлен файл modal.js вместо main.js

---
+++ src/js/modal.js
@@ -0,0 +1,12 @@
+$(document).ready(function() { // Проверяем, загрузилрсь ли дерево страницы, и если да - запускаем функцию
+  var button = $('#button'); // Вместо var button = document.querySelector('#button');
+  var modal = $('#modal');
+  var close = $('#close');
+
+  button.on('click', function() { // on - отлавливает клик, и запускает функцию: 
+    modal.addClass('modal_active');
+  });
+  close.on('click', function() {
+    modal.removeClass('modal_active');
+  });
+});
